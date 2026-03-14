e6f299b5c1543a5227508a2329cdf6944101c14f
Ayax Arcos <ayaxarcos@gmail.com>
2026-03-13 19:44:35 -0600

    Agregar implementacion de syscall read en scan_string.asm
    Se completó para leer una cadena del usuario en un buffer, asegurando el 0 final (y normalmente quitando el '\n' si aparece) usando la syscall read.

--- src/scan_string.asm
+++ src/scan_string.asm
@@ -13,10 +13,53 @@ scan_string:
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. syscall read
-    ; 2. guardar en buffer
-    ; 3. agregar terminador 0
+push ecx
+    push edx
+    push esi
+    push edi
+
+    mov esi, eax        
+    mov edx, ebx        
+    cmp edx, 1
+    jbe .solo_cero      
+
+    dec edx
+
+    mov eax, 3          
+    mov ebx, 0          
+    mov ecx, esi        
+    int 0x80 
+
+    cmp eax, 1
+    jl .poner_cero
+
+    mov edi, esi
+    add edi, eax
+    mov byte [edi], 0
+
+    mov edi, esi
+    add edi, eax
+    dec edi
+    cmp byte [edi], 10      
+    jne .fin
+    mov byte [edi], 0
+    dec eax
+    jmp .fin
+
+.solo_cero:
+    mov byte [esi], 0
+    xor eax, eax
+    jmp .fin
+
+.poner_cero:
+    mov byte [esi], 0
+    xor eax, eax
+
+.fin:
+    pop edi
+    pop esi
+    pop edx
+    pop ecx
 
     mov esp, ebp
     pop ebp
