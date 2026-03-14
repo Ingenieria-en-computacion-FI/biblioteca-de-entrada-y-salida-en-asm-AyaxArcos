91bd2e61cde8174980c4fa44c18785f8abe53dde
Ayax Arcos <ayaxarcos@gmail.com>
2026-03-13 19:34:06 -0600

    Implementar el calculo de string length y syscall write
    Se completó para imprimir una cadena terminada en 0, primero calculando su longitud y luego usando la syscall write para imprimirla completa.

--- src/print_string.asm
+++ src/print_string.asm
@@ -13,9 +13,31 @@ print_string:
     push ebp
     mov ebp, esp
 
-    ; TODO:
-    ; 1. calcular longitud
-    ; 2. syscall write
+    push ebx
+    push ecx
+    push edx
+    push esi
+
+    mov esi, eax        
+    
+    xor edx, edx        
+.len_loop:
+    cmp byte [esi + edx], 0
+    je .len_done
+    inc edx
+    jmp .len_loop
+
+.len_done:
+    mov eax, 4          
+    mov ebx, 1          
+    mov ecx, esi
+
+    int 0x80
+
+    pop esi
+    pop edx
+    pop ecx
+    pop ebx
 
     mov esp, ebp
     pop ebp
